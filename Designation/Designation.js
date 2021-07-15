ff4997bd1de5557df291cbb401d7887925ed57c9
Kyouis <45875365+Kyouis@users.noreply.github.com>
2021-07-15 22:36:56 +0200

    picker designation

--- Designation/Designation.js
+++ Designation/Designation.js
@@ -1,9 +1,40 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {Picker, Text, TextInput, TouchableOpacity, View} from "react-native";
 import * as SQLite from "expo-sqlite";
+import {useFocusEffect} from "@react-navigation/native";
+
+const db = SQLite.openDatabase('Agora');
 
 const Designation = () => {
-    return <Text>Test</Text>
+    const [data, setData] = useState([]);
+    const [comp, setComp] = useState();
+    useFocusEffect( () => {
+        useCallback(
+            () => {
+                db.transaction((tx) => {
+                    tx.executeSql("SELECT * FROM COMPARTIMENT", [],(tx, rs) => {
+                        let res = [];
+                        for (let i = 0; i<rs.rows.length; i++) {
+                            res.push(rs.rows.item(i));
+                        }
+                        setData(res);
+                    }, () => {}, (tx, e) => console.log(e))
+                }, (e) => console.log(e+ 'transSelectDesi'))
+            }, []
+        );
+    });
+    return (
+        <View>
+            <Picker
+                selectedValue={comp}
+                onValueChange={(v)=>{setComp(v)}}>
+                {data.map((item, index) => {
+                    console.log(item);
+                    return (<Picker.Item label={item.refLocalisation} value={item.idComp} key={index}/>)
+                })}
+            </Picker>
+        </View>
+    );
 }
 
 export default Designation;
